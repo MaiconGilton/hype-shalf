ae71c99d7cf9995d5cf6d6da0126b907852a3073
Maicon Gilton <maicongilton@gmail.com>
2026-02-12 21:59:58 -0300

    refactor: move genre color map to inside movie-grid

--- src/components/movie-grid.tsx
+++ src/components/movie-grid.tsx
@@ -1,8 +1,18 @@
-import { Movie, GENRE_COLORS } from '@/types/movie'
+import { Genre, Movie } from '@/types/movie'
 
 interface MovieGridProps {
   movies: Movie[]
 }
+
+export const GENRE_COLORS: Record<Genre, string> = {
+  'Horror': 'bg-red-100 text-red-800 dark:bg-red-900/30 dark:text-red-300',
+  'Action': 'bg-orange-100 text-orange-800 dark:bg-orange-900/30 dark:text-orange-300',
+  'Comedy': 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900/30 dark:text-yellow-300',
+  'Drama': 'bg-purple-100 text-purple-800 dark:bg-purple-900/30 dark:text-purple-300',
+  'Sci-Fi': 'bg-blue-100 text-blue-800 dark:bg-blue-900/30 dark:text-blue-300',
+  'Thriller': 'bg-green-100 text-green-800 dark:bg-green-900/30 dark:text-green-300',
+}
+
 
 export function MovieGrid({ movies }: MovieGridProps) {
   return (
